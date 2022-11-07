06d6131846447e057439f7e991e429ec1df83e8f
Yoel RIDGWAY-LOPEZ <yridgway@e3r1p14.42.fr>
2022-11-07 20:01:42 +0100

    fixed problem with philos doing things after one has died

--- sources/philo.c
+++ sources/philo.c
@@ -6,7 +6,7 @@
 /*   By: yridgway <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/28 18:03:13 by yridgway          #+#    #+#             */
-/*   Updated: 2022/11/03 18:22:55 by yridgway         ###   ########.fr       */
+/*   Updated: 2022/11/07 19:55:21 by yridgway         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,28 @@
 
 void	check_death(t_data *dat, t_philo *philo)
 {
-	pthread_mutex_lock(&dat->mutex);
+//	pthread_mutex_lock(&dat->mutex);
 	philo->is_dead = dat->death;
-	pthread_mutex_unlock(&dat->mutex);
+//	pthread_mutex_unlock(&dat->mutex);
 	if (!philo->is_dead && ft_time(philo->begin_time)
 		- philo->last_meal > philo->die_time)
 	{
-		pthread_mutex_lock(&dat->mutex);
+//		pthread_mutex_lock(&dat->mutex);
 		printf("%ld %d died\n", ft_time(philo->begin_time), philo->id);
 		dat->death = 1;
-		pthread_mutex_unlock(&dat->mutex);
+//		pthread_mutex_unlock(&dat->mutex);
 	}
-	pthread_mutex_lock(&dat->mutex);
+//	pthread_mutex_lock(&dat->mutex);
 	philo->is_dead = dat->death;
-	pthread_mutex_unlock(&dat->mutex);
+//	pthread_mutex_unlock(&dat->mutex);
 }
 
 void	philo_eat(t_data *dat, t_philo *philo)
 {
 	pthread_mutex_lock(&dat->mutex);
-	printf("%ld %d is eating\n", ft_time(philo->begin_time), philo->id);
+	check_death(dat, philo);
+	if (!philo->is_dead)
+		printf("%ld %d is eating\n", ft_time(philo->begin_time), philo->id);
 	pthread_mutex_unlock(&dat->mutex);
 	usleep(philo->eat_time * 1000);
 	pthread_mutex_lock(&dat->mutex);
@@ -53,8 +55,8 @@ void	ft_pickup(t_data *dat, t_philo *philo)
 	fork2 = 1;
 	while (!philo->is_dead && (fork1 || fork2))
 	{
-		check_death(dat, philo);
 		pthread_mutex_lock(&dat->mutex);
+		check_death(dat, philo);
 		if (!philo->is_dead && dat->forks[philo->id - 1])
 		{
 			dat->forks[philo->id - 1] = 0;
@@ -64,8 +66,8 @@ void	ft_pickup(t_data *dat, t_philo *philo)
 		}
 		pthread_mutex_unlock(&dat->mutex);
 		usleep(1);
-		check_death(dat, philo);
 		pthread_mutex_lock(&dat->mutex);
+		check_death(dat, philo);
 		if (!philo->is_dead && dat->forks[philo->id % dat->num_philos])
 		{
 			dat->forks[philo->id % dat->num_philos] = 0;
@@ -74,7 +76,6 @@ void	ft_pickup(t_data *dat, t_philo *philo)
 			fork2 = 0;
 		}
 		pthread_mutex_unlock(&dat->mutex);
-		check_death(dat, philo);
 	}
 }
 
@@ -86,19 +87,25 @@ void	philo_sleep(t_data *dat, t_philo *philo)
 	if (!philo->is_dead)
 	{
 		pthread_mutex_lock(&dat->mutex);
-		printf("%ld %d is sleeping\n", ft_time(philo->begin_time), philo->id);
+		check_death(dat, philo);
+		if (!philo->is_dead)
+			printf("%ld %d is sleeping\n", ft_time(philo->begin_time), philo->id);
 		pthread_mutex_unlock(&dat->mutex);
 	}
 	endtime = ft_time(philo->begin_time) + philo->sleep_time;
 	while (!philo->is_dead && ft_time(philo->begin_time) < endtime)
 	{
 		usleep(1000);
+		pthread_mutex_lock(&dat->mutex);
 		check_death(dat, philo);
+		pthread_mutex_unlock(&dat->mutex);
 	}
 	if (!philo->is_dead)
 	{
 		pthread_mutex_lock(&dat->mutex);
-		printf("%ld %d is thinking\n", ft_time(philo->begin_time), philo->id);
+		check_death(dat, philo);
+		if (!philo->is_dead)
+			printf("%ld %d is thinking\n", ft_time(philo->begin_time), philo->id);
 		pthread_mutex_unlock(&dat->mutex);
 	}
 }
@@ -106,7 +113,9 @@ void	philo_sleep(t_data *dat, t_philo *philo)
 void	philo_does_things(t_data *dat, t_philo *philo)
 {
 	ft_pickup(dat, philo);
+	pthread_mutex_lock(&dat->mutex);
 	check_death(dat, philo);
+	pthread_mutex_unlock(&dat->mutex);
 	if (!philo->is_dead && philo->times_to_eat)
 	{
 		philo->last_meal = ft_time(philo->begin_time);
@@ -115,5 +124,7 @@ void	philo_does_things(t_data *dat, t_philo *philo)
 		if (philo->times_to_eat != 0)
 			philo_sleep(dat, philo);
 	}
+	pthread_mutex_lock(&dat->mutex);
 	check_death(dat, philo);
+	pthread_mutex_unlock(&dat->mutex);
 }
